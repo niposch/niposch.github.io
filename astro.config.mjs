9dd0e70a8a64d6036a88aaec84e3137830725665
niposch <niposch@gmail.com>
2024-10-26 13:28:57 +0200

    added deploy yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://niposch.github.io',
+  output: 'static',
   integrations: [
     vue(),
     tailwind({ 
